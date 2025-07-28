b315f8bd323a863cc3581a9462cfc7740bfd65c2
Painture <aidounisirina@gmail.com>
2025-07-28 12:22:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,25 +13,3 @@ if (urname == "Sirina") {
 else {
 	title.style.color = "white";
 }
-
-
-
-// let age = prompt("How old r u?")
-
-// if (age >= 14) {
-// 	alert("welcome to JIT")
-// } 
-// else {
-// 	alert("Come back in a few years")
-// }
-
-
-
-
-// const timestamp = Date.now();
-
-// const currentDate = new Date(timestamp);
-
-// //Formatting date 
-// const formattedDate = currentDate.toLocaleString();
-// alert(`Current Date and Time: ${formattedDate}`);
